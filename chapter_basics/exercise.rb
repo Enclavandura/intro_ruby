2453f8454aa0c0c2c537d703eb8d586ef00c2237
Matic Pusser <matic.pusser@gmail.com>
2020-11-29 23:27:11 +0100

    all exercises

--- chapter_basics/exercise.rb
+++ chapter_basics/exercise.rb
@@ -1,4 +1,31 @@
+# exercise 1
 first = "Matic"
 last = " Pusser"
 first_last = first + last
 puts first_last
+
+
+
+# exercise 3
+movie_years = {:troy => "2004", :gladiator => "2000", :braveheart => "1996"}
+puts movie_years[:troy]
+puts movie_years[:gladiator]
+puts movie_years[:braveheart]
+
+# exercise 4
+movie_years_array = ["2004", "2000", "1996"]
+puts movie_years_array[0]
+puts movie_years_array[1]
+puts movie_years_array[2]
+
+# exercise 5
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+
+# exercise 6
+puts 1.3**2
+puts 2.5**2
+puts 7.07**2
+
