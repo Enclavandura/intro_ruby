59347a56268fd594e404f0755be564959da517e0
Matic Pusser <matic.pusser@gmail.com>
2020-11-30 13:56:08 +0100

    age

---
+++ chapter_variables/age.rb
@@ -0,0 +1,9 @@
+puts "Enter your age:"
+age_input = gets.chomp.to_i
+
+
+puts "In 10 years you will be #{age_input + 10}"
+puts "In 20 years you will be #{age_input + 20}"
+puts "In 30 years you will be #{age_input + 30}"
+puts "In 40 years you will be #{age_input + 40}"
+puts "in 50 years you will be #{age_input + 50}"
